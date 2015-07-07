4d5fd81303e223379718b7a255286d922a0662ea
Svartfax <ok@blay.se>
2015-07-07 23:24:20 +0200

    symlink

---
+++ symlink.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ln -s ~/dotfiles/ackrc ~/ackrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/readme.md ~/.readme.md
+ln -s ~/dotfiles/shell ~/.shell
+ln -s ~/dotfiles/timetrap.yml ~/.timetrap.yml
+ln -s ~/dotfiles/tmux-osx.conf ~/.tmux-osx.conf
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/todo ~/.todo
+ln -s ~/dotfiles/todo.actions.d ~/.todo.actions.d
+ln -s ~/dotfiles/todo.cfg ~/.todo.cfg
+ln -s ~/dotfiles/todofix.cfg ~/.todofix.cfg
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
