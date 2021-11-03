8ebeb525b96224eee041e3700e247f81a88e8e45
svartfax <ok@blay.se>
2021-11-04 00:25:51 +0100

    simple-fix

--- shell/simple-present.sh
+++ shell/simple-present.sh
@@ -3,7 +3,7 @@
 # How long is the ice melting in miliseconds?
 ICEMELTING=$(expr 4 \* 60 \* 1000)
 
-# How many moments is the melting captured into?
+# How many moments is the melting captured into? (starts from 0)
 MOMENTS=999
 
 # How many images in each book?
@@ -18,16 +18,16 @@ for j in $(seq 0 $MOMENTS)
 do 
     # The time of the captured moment is the duration of a present times the moment number with three random decimals added.
     MOMENT=$(expr $PRESENT \* $j)ms
-# The future is the moment number divided by number of futures plus one since there is no future zero.
+# The number of books is the amount of moments divided by number of images  plus one since there is no book zero.
     BOOK=$(($j/$IMAGES+1))
 
-    # If the moment is evenly divisble by the number of futures, make a new future.
+    # If the moment is evenly divisble by the number of books, make a new book.
     if 
         [ $(( j % $IMAGES )) -eq 0 ]; then
             mkdir $BOOK
     fi
 
-    # Capture a moment at the time of a present from the ice melting and place it in its future, named after the timestamp of its moment.
+    # Capture a moment at the time of a present from the ice melting and place it in its book, named after the timestamp of its moment.
     ffmpeg -ss $MOMENT -i $1 -frames:v 1 $BOOK/$MOMENT.jpg
 
 # All operations are done, object has melted.
