59d3cc44d4138cae46dfa986ee73ae049a6f17b5
svartfax <ok@blay.se>
2021-11-04 00:29:58 +0100

    simple-fix

--- shell/simple-present.sh
+++ shell/simple-present.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# How long is the ice melting in miliseconds?
-ICEMELTING=$(expr 4 \* 60 \* 1000)
+# How long is the ice artwork melting in miliseconds?
+MELTING=$(expr 4 \* 60 \* 1000)
 
 # How many moments is the melting captured into? (starts from 0)
 MOMENTS=999
@@ -10,7 +10,7 @@ MOMENTS=999
 IMAGES=5
 
 # This gives the duration of a present.
-PRESENT=$(expr $ICEMELTING / $MOMENTS)
+PRESENT=$(expr $MELTING / $MOMENTS)
 
 # Iterate over moments and perform the operations.
 for j in $(seq 0 $MOMENTS)
@@ -18,16 +18,16 @@ for j in $(seq 0 $MOMENTS)
 do 
     # The time of the captured moment is the duration of a present times the moment number with three random decimals added.
     MOMENT=$(expr $PRESENT \* $j)ms
-# The number of books is the amount of moments divided by number of images  plus one since there is no book zero.
+    # The number of books is the amount of moments divided by number of images  plus one since there is no book zero.
     BOOK=$(($j/$IMAGES+1))
 
-    # If the moment is evenly divisble by the number of books, make a new book.
+    # If the moment is evenly divisible by the number of books, make a new book.
     if 
         [ $(( j % $IMAGES )) -eq 0 ]; then
             mkdir $BOOK
     fi
 
-    # Capture a moment at the time of a present from the ice melting and place it in its book, named after the timestamp of its moment.
+    # Capture a moment at the time of a present from the melting and place it in a book, named after the timestamp of its moment.
     ffmpeg -ss $MOMENT -i $1 -frames:v 1 $BOOK/$MOMENT.jpg
 
 # All operations are done, object has melted.
